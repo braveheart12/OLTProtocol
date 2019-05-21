82c6eb3a62099879c4e8afd64b0786cac4375df6
Harschad Patil <harshad@oneloedger.io>
2019-05-21 16:43:42 -0400

    adding code to enable cpu profiler

--- cmd/olfullnode/main.go
+++ cmd/olfullnode/main.go
@@ -5,6 +5,29 @@
 */
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
 func main() {
+
+	rs := rand.NewSource(time.Now().UnixNano())
+	f, err := os.Create(fmt.Sprintf("/tmp/profile%d_cpu.pprof", rs.Int63()))
+	if err != nil {
+		fmt.Println(err, ": error opening file")
+		return
+	}
+	err = pprof.StartCPUProfile(f)
+	if err != nil {
+		fmt.Println(err, ": err starting cpu profiling")
+		return
+	}
+
 	Execute() // Pass control to Cobra
+
+	pprof.StopCPUProfile()
 }
